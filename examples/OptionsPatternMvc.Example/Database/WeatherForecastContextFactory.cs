156fbdbdd01ec276bac65451dea04d145619c44a
Thomas Harold <tgharold@gmail.com>
2020-03-17 20:53:21 -0400

    create a dbcontext factor

--- examples/OptionsPatternMvc.Example/Database/WeatherForecastContextFactory.cs
+++ examples/OptionsPatternMvc.Example/Database/WeatherForecastContextFactory.cs
@@ -1,12 +1,22 @@
 using System.Data.Entity.Infrastructure;
+using Microsoft.Extensions.Options;
+using OptionsPatternMvc.Example.Settings;
 
 namespace OptionsPatternMvc.Example.Database
 {
-    public class WeatherForecastContextFactory : IDbContextFactory< WeatherForecastContext>
+    public class WeatherForecastContextFactory : IDbContextFactory<WeatherForecastContext>
     {
+        private readonly IOptionsMonitor<ConnectionStringsSettings> _connectionStringsAccessor;
+
+        public WeatherForecastContextFactory(IOptionsMonitor<ConnectionStringsSettings> connectionStringsAccessor)
+        {
+            _connectionStringsAccessor = connectionStringsAccessor;
+        }
+
         public WeatherForecastContext Create()
         {
-            return new WeatherForecastContext();
+            var connectionString = _connectionStringsAccessor.CurrentValue.OpvExample1;
+            return new WeatherForecastContext(connectionString);
         }
     }
 }
