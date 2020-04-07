ac08db52875c9ce24187b95196500435ec1b12dc
Thomas Harold <tgharold@gmail.com>
2020-04-07 14:16:15 -0400

    move the representation to sub-folder

--- examples/OptionsPatternMvc.Dapper/Controllers/WeatherForecastController.cs
+++ examples/OptionsPatternMvc.Dapper/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OptionsPatternMvc.Dapper.Representations;
 
 namespace OptionsPatternMvc.Dapper.Controllers
 {
@@ -24,10 +25,10 @@ namespace OptionsPatternMvc.Dapper.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecastRepresentation> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecastRepresentation
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
