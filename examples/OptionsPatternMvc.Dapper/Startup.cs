f3db9a62e5629fe3784c643b14d445ec71c14200
Thomas Harold <tgharold@gmail.com>
2020-04-07 16:11:19 -0400

    wire up a IDbConnection as Scoped

--- examples/OptionsPatternMvc.Dapper/Startup.cs
+++ examples/OptionsPatternMvc.Dapper/Startup.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Data;
+using System.Data.SQLite;
+using FluentMigrator.Runner;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using OptionsPatternMvc.Dapper.Extensions;
 using OptionsPatternMvc.Dapper.Settings;
 using OptionsPatternValidation;
@@ -28,6 +32,13 @@ namespace OptionsPatternMvc.Dapper
             services.AddOpvDapperFluentMigrator();
 
             services.AddSingleton<Random>();
+
+            services.AddScoped<IDbConnection>(provider =>
+            {
+                var connectionString =
+                    provider.GetRequiredService<IOptions<ConnectionStringsSettings>>().Value.OpvDapper;
+                return new SQLiteConnection(connectionString);
+            });
             
             services.AddControllers();
         }
