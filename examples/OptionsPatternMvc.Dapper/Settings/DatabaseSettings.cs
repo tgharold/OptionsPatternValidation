3c2cd13d9a57cbe56ce1c759dadd203eacefd94d
Thomas Harold <tgharold@gmail.com>
2020-04-07 14:16:43 -0400

    set the section name in appsettings.json

--- examples/OptionsPatternMvc.Dapper/Settings/DatabaseSettings.cs
+++ examples/OptionsPatternMvc.Dapper/Settings/DatabaseSettings.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using OptionsPatternValidation;
 
 namespace OptionsPatternMvc.Dapper.Settings
 {
+    [SettingsSectionName("Database")]
     public class DatabaseSettings
     {
         [Required]
