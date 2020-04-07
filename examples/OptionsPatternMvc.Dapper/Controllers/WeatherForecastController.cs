aeee4cf353aa3a9d3a27806210c5351d4a16086f
Thomas Harold <tgharold@gmail.com>
2020-04-07 15:51:21 -0400

    Add CreateForecastForDay(), wire up Random
    Should only have a single instance of Random

--- examples/OptionsPatternMvc.Dapper/Controllers/WeatherForecastController.cs
+++ examples/OptionsPatternMvc.Dapper/Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OptionsPatternMvc.Dapper.Models;
 using OptionsPatternMvc.Dapper.Representations;
 
 namespace OptionsPatternMvc.Dapper.Controllers
@@ -18,10 +19,15 @@ namespace OptionsPatternMvc.Dapper.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly Random _random;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger,
+            Random random
+            )
         {
             _logger = logger;
+            _random = random;
         }
 
         [HttpGet]
@@ -35,6 +41,17 @@ namespace OptionsPatternMvc.Dapper.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+        
+        private WeatherForecast CreateForecastForDay(DateTime date)
+        {
+            return new WeatherForecast
+            {
+                ForecastTime = DateTime.UtcNow,
+                Date = date,
+                TemperatureC = _random.Next(-20, 55),
+                Summary = Summaries[_random.Next(Summaries.Length)]
+            };
         }
     }
 }
--- examples/OptionsPatternMvc.Dapper/Startup.cs
+++ examples/OptionsPatternMvc.Dapper/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -25,6 +26,8 @@ namespace OptionsPatternMvc.Dapper
             services.AddValidatedSettings<DatabaseSettings>(_configuration);
 
             services.AddOpvDapperFluentMigrator();
+
+            services.AddSingleton<Random>();
             
             services.AddControllers();
         }
