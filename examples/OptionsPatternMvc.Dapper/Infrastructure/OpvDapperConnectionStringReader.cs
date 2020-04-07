78b984abe6da51ea925d691a1ba943a888f4f9bd
Thomas Harold <tgharold@gmail.com>
2020-04-07 14:21:09 -0400

    add details about how FM library uses this

--- examples/OptionsPatternMvc.Dapper/Infrastructure/OpvDapperConnectionStringReader.cs
+++ examples/OptionsPatternMvc.Dapper/Infrastructure/OpvDapperConnectionStringReader.cs
@@ -16,14 +16,22 @@ namespace OptionsPatternMvc.Dapper.Infrastructure
         {
             _connectionStringAccessor = connectionStringAccessor;
         }
-        
-        public string GetConnectionString(string connectionStringOrName)
+
+        /// <summary>Returns the connection string.</summary>
+        /// <param name="_">In current versions of FluentMigrator, this will never be
+        /// a connection string, but may be the name of a FM processor.  There is
+        /// code in FM that looks for a prefix of "SqlServer" on the processor
+        /// name to determine whether it is executing against Microsoft SQL Server.
+        /// This value defaults to null if not specified during setup.</param>
+        public string GetConnectionString(string _)
         {
             return _connectionStringAccessor.Value.OpvDapper;
         }
 
         /// <summary>Use a priority of at least 100 as there is code in FM.Core that sets up a
-        /// default IConnectionStringReader with a priority of 100.</summary>
+        /// default IConnectionStringReader with a priority of 100.  See the FluentMigrator
+        /// ConnectionStringAccessor class for details on how priority is used.  Higher
+        /// values get used first.</summary>
         public int Priority => 1000;
     }
 }
