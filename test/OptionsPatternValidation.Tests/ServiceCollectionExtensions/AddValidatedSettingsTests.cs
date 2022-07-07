4b3c3b1c0b6a4b34f75780339786cb4e5c4ddd7a
Thomas Harold <tom.harold@ritterim.com>
2022-07-07 11:47:17 -0400

    Add some tests around AddValidatedSettings()
    Verify that IOptions / IOptionsSnapshot / IOptionsMonitor all behave
    as expected.  (Just to double-check a bug in another project.)

--- test/OptionsPatternValidation.Tests/ServiceCollectionExtensions/AddValidatedSettingsTests.cs
+++ test/OptionsPatternValidation.Tests/ServiceCollectionExtensions/AddValidatedSettingsTests.cs
@@ -13,10 +13,9 @@ namespace OptionsPatternValidation.Tests.ServiceCollectionExtensions
     public class AddValidatedSettingsTests
     {
         [Fact]
-        public void Wires_up_settings_from_string()
+        public void Wires_up_settings_from_string_as_IOptions()
         {
             var services = new ServiceCollection();
-
             
             const string json = @"
 {
@@ -39,6 +38,58 @@ namespace OptionsPatternValidation.Tests.ServiceCollectionExtensions
             Assert.True(result.BooleanB);
         }
 
+        [Fact]
+        public void Wires_up_settings_from_string_as_IOptionsSnapshot()
+        {
+            var services = new ServiceCollection();
+            
+            const string json = @"
+{
+""AttributeValidated"": {
+    ""IntegerA"": 23,
+    ""BooleanB"": false
+  }
+}
+                ";
+
+            var configuration = ConfigurationTestBuilder.BuildFromJsonString(json);
+            
+            services.AddValidatedSettings<AttributeValidatedSettings>(configuration);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var result = serviceProvider.GetRequiredService<IOptionsSnapshot<AttributeValidatedSettings>>().Value;
+
+            Assert.NotNull(result);
+            Assert.Equal(23, result.IntegerA);
+            Assert.False(result.BooleanB);
+        }
+
+        [Fact]
+        public void Wires_up_settings_from_string_as_IOptionsMonitor()
+        {
+            var services = new ServiceCollection();
+            
+            const string json = @"
+{
+""AttributeValidated"": {
+    ""IntegerA"": 88,
+    ""BooleanB"": false
+  }
+}
+                ";
+
+            var configuration = ConfigurationTestBuilder.BuildFromJsonString(json);
+            
+            services.AddValidatedSettings<AttributeValidatedSettings>(configuration);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var result = serviceProvider.GetRequiredService<IOptionsMonitor<AttributeValidatedSettings>>().CurrentValue;
+
+            Assert.NotNull(result);
+            Assert.Equal(88, result.IntegerA);
+            Assert.False(result.BooleanB);
+        }
+
         /// <summary>Demonstrate that AddValidatedSettings can work with POCOs that have no 
         /// DataAnnotation validation attributes on any properties.</summary>
         [Theory]
@@ -59,7 +110,7 @@ namespace OptionsPatternValidation.Tests.ServiceCollectionExtensions
         }
         
         [Fact]
-        public void Catches_validation_error_for_Test1_file()
+        public void Catches_validation_error_for_Test1_file_using_IOptions()
         {
             var services = new ServiceCollection();
             
@@ -76,6 +127,54 @@ namespace OptionsPatternValidation.Tests.ServiceCollectionExtensions
             Action act = () =>
             {
                 var _ = optionsAccessor.Value;
+            };
+            
+            var exception = Assert.Throws<OptionsValidationException>(act);
+            Assert.StartsWith("Validation failed for members: 'IntegerA'", exception.Message);
+        }
+
+        [Fact]
+        public void Catches_validation_error_for_Test1_file_using_IOptionsSnapshot()
+        {
+            var services = new ServiceCollection();
+            
+            var configuration = ConfigurationTestBuilder.BuildFromEmbeddedResource(
+                JsonIndex.AttributeValidated.Test1);
+            
+            services.AddValidatedSettings<AttributeValidatedSettings>(configuration);
+            
+            var serviceProvider = services.BuildServiceProvider();
+
+            // this call will work, because we're just getting the accessor singleton
+            var optionsAccessor = serviceProvider.GetRequiredService<IOptionsSnapshot<AttributeValidatedSettings>>();
+            // the next call will throw an exception because we're accessing the .Value instance
+            Action act = () =>
+            {
+                var _ = optionsAccessor.Value;
+            };
+            
+            var exception = Assert.Throws<OptionsValidationException>(act);
+            Assert.StartsWith("Validation failed for members: 'IntegerA'", exception.Message);
+        }
+
+        [Fact]
+        public void Catches_validation_error_for_Test1_file_using_IOptionsMonitor()
+        {
+            var services = new ServiceCollection();
+            
+            var configuration = ConfigurationTestBuilder.BuildFromEmbeddedResource(
+                JsonIndex.AttributeValidated.Test1);
+            
+            services.AddValidatedSettings<AttributeValidatedSettings>(configuration);
+            
+            var serviceProvider = services.BuildServiceProvider();
+
+            // this call will work, because we're just getting the accessor singleton
+            var optionsAccessor = serviceProvider.GetRequiredService<IOptionsMonitor<AttributeValidatedSettings>>();
+            // the next call will throw an exception because we're accessing the .CurrentValue instance
+            Action act = () =>
+            {
+                var _ = optionsAccessor.CurrentValue;
             };
             
             var exception = Assert.Throws<OptionsValidationException>(act);
